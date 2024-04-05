57eda5225725ff0fcdde5c3b4f4774d588961d5c
itzDM <bitdev20@gmail.com>
2024-04-05 15:54:10 +0530

    feat(api):mvc

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from API!");
+  res.send("Hello from API! ");
 });
 
 app.listen(process.env.PORT || 3000, () => {
