0ac92982f2cf0c8e7dff40d33b1fbc363c512c59
itzDM <bitdev20@gmail.com>
2024-04-23 01:09:22 +0530

    create post work fine

--- client/src/pages/create/create.tsx
+++ client/src/pages/create/create.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import { apiRequest } from "../../lib/apiRequest";
 import "./create.css";
 export default function CreatePost() {
+  const [error, SetError] = useState("");
+  const [message, SetMessage] = useState("");
+  const handelSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const { image, ...data } = Object.fromEntries(formData.entries());
+    console.log(image);
+    const response = await apiRequest("/post", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.message) {
+      (e.target as HTMLFormElement).reset();
+      SetMessage(response.message);
+    }
+    SetError(response.error);
+  };
   return (
     <div>
       <section className="create">
-        <form className="createForm">
+        <form className="createForm" onSubmit={handelSubmit}>
           <h2>Create A New real-estate</h2>
           <input
             type="text"
@@ -36,11 +58,13 @@ export default function CreatePost() {
             required
           />
           <select name="type">
-            <option value="land">Lands</option>
+            <option value="lands">Lands</option>
             <option value="room">Room</option>
           </select>
-          <input type="file" name="image" accept=" image/*" required />
+          <input type="file" name="image" accept=" image/*" />
           <button type="submit">Create</button>
+          {error && <p className="error">{error}</p>}
+          {message && <p className="message">{message}</p>}
         </form>
       </section>
     </div>
